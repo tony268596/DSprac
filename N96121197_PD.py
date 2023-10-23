90693f970c4c5a6168c3044b907d1280be510ab6
unknown <tony268596@gmail.com>
2023-10-23 11:13:44 +0800

    PD 231 321 don't work

--- N96121197_PD.py
+++ N96121197_PD.py
@@ -1,16 +1,35 @@
 
 def poker(data):
-    # # B
-    # stack1 = []
-    # # 牌盒
-    # stack2 = []
-    # if data[0] == 1:
-    #     stack1
-    if data == [2, 3, 1]:
-        return "No"
-    return "Yes"
+    # B
+    stack1 = []
+    # 牌盒
+    stack2 = []
+    check = 1
+    while data:
+        if data[0] == check:
+            stack1.append(data.pop(0))
+            check += 1
+        elif stack2:
+            if check == stack2[-1]:
+                stack1.append(stack2.pop())
+        else:
+            tmp = data.pop(0)
+            if is_valid(stack2, tmp):
+                stack2.append(tmp)
+    while stack2:
+        stack1.append(stack2.pop())
+    if stack1 == [1,2,3]:
+        return "Yes"
+    return "No"
 
+def is_valid(arr, a):
+    if not arr: return 1
 
+    if arr[-1] < a:
+        return 0
+    return 1
+    
 tmp = input()
-data = [int(i) for i in tmp.split()]
-print(poker(data))
+test = [int(i) for i in tmp.split()]
+print(test)
+print(poker(test))
