11f281488ecc42427c6e379a87d23325054f7bc9
tony268596 <tony268596@gmail.com>
2023-12-07 18:13:14 +0800

    add DSFP PB

---
+++ DSFprac/N96121197_PB.py
@@ -0,0 +1,26 @@
+INF = 9999
+
+path_map = [
+    [0, 50, 60, INF, INF, INF, INF],
+    [50, 0, INF, 120, 90, INF, INF],
+    [60, INF, 0, INF, INF, 50, INF],
+    [INF, 120, INF, 0, INF, 80, 70],
+    [INF, 90, INF, INF, 0, INF, 40],
+    [INF, INF, 50, 80, INF, 0, 140],
+    [INF, INF, INF, 70, 40, 140, 0]
+]
+
+def minflow():
+    length = len(path_map)
+    for k in range(length):
+        for i in range(length):
+            for j in range(length):
+                path_map[i][j] = min(path_map[i][j], max(path_map[i][k], path_map[k][j]))
+
+minflow()
+
+
+for i in range(int(input())):
+    tmp = input()
+    data = [int(_) for _ in tmp.split()]
+    print(path_map[data[0]-1][data[1]-1])
